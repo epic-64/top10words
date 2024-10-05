5eb4845fbd90905791b8540d50f3ae2ff7e02965
William Raendchen <william.raendchen@gmail.com>
2024-10-05 04:15:30 +0200

    refactor

--- src/main/scala/composer/Types.scala
+++ src/main/scala/composer/Types.scala
@@ -3,25 +3,32 @@ package composer
 import upickle.default.*
 import upickle.implicits.key
 
-case class Package(packageName: String, packageVersion: String)derives ReadWriter {
+case class Package(packageName: String, packageVersion: String) derives ReadWriter:
     override def toString: String = s"$packageName:$packageVersion"
-}
 
 case class PackageSummary(packageName: String, usageCount: Int, versions: Map[String, List[String]])
 
 sealed trait PackageListState
-class Unsorted extends PackageListState
 class Compiled extends PackageListState
 class Sorted extends PackageListState
 
-case class PackageList[s <: PackageListState](items: List[PackageSummary])
+case class PackageList[s <: PackageListState](items: List[PackageSummary]) {
+    def print(): Unit = {
+        items.foreach((item: PackageSummary) => {
+            println(s"${item.packageName} - ${item.usageCount}")
+            item.versions.foreach((version, projectList) => {
+                println(s"  $version - ${projectList.size}")
+                projectList.foreach((project) => {
+                    println(s"    $project")
+                })
+            })
+        })
+    }
+}
 
 object CompiledPackageList:
     def sort(list: PackageList[Compiled]): PackageList[Sorted] =
-        PackageList[Sorted](list.items
-            .sortBy(_.packageName).reverse
-            .sortBy(_.usageCount).reverse
-        )
+        PackageList[Sorted](list.items.sortBy(_.packageName).reverse.sortBy(_.usageCount).reverse)
 
     private def getSummaries(packageName: String, packageList: List[Package]): PackageSummary = {
         val versions = packageList
@@ -65,22 +72,11 @@ case class ComposerFile(
     name: String,
     @key("require") packages: Map[String, String],
     @key("require-dev") dev: Option[Map[String, String]] = None
-)derives ReadWriter {
-    def combinedList: List[Package] =
-        packagesAsList ++ devAsList
-
-    def packagesAsList: List[Package] =
-        packages
-            .map { case (pkg, version) => Package(pkg, version) }
-            .toList
-
-    def devAsList: List[Package] =
-        dev
-            .getOrElse(Map.empty)
-            .map { case (pkg, version) => Package(pkg, version) }
-            .toList
+) derives ReadWriter {
+    def combinedList: List[Package] = packagesAsList ++ devAsList
+    private def packagesAsList: List[Package] = packages.map((pkg, version) => Package(pkg, version)).toList
+    private def devAsList: List[Package] = dev.getOrElse(Map.empty).map((pkg, version) => Package(pkg, version)).toList
 }
 
 object ComposerFile:
-    def fromJson(jsonContent: String): ComposerFile =
-        read[ComposerFile](jsonContent)
+    def fromJson(jsonContent: String): ComposerFile = read[ComposerFile](jsonContent)
--- src/main/worksheets/composer-worksheet.sc
+++ src/main/worksheets/composer-worksheet.sc
@@ -6,10 +6,10 @@ import scala.collection.immutable.HashMap
 
 val repo = new ComposerRepository()
 
-val files = repo.all()
+val files: List[ComposerFile] = repo.all()
 
 val compiledList = CompiledPackageList.fromFiles(files)
 val sortedList = CompiledPackageList.sort(compiledList)
 
 CompiledPackageList.printInfo(files.size, compiledList.items.size, compiledList.items.flatMap(_.versions.keys).size)
-CompiledPackageList.print(sortedList)
+sortedList.print()
