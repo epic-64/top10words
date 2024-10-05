7cd991a68f30c39a6182380402f2524a52af4f44
William Raendchen <william.raendchen@gmail.com>
2024-10-05 03:29:10 +0200

    refactor

--- src/main/scala/composer/Types.scala
+++ src/main/scala/composer/Types.scala
@@ -7,7 +7,7 @@ case class Package(packageName: String, packageVersion: String)derives ReadWrite
     override def toString: String = s"$packageName:$packageVersion"
 }
 
-case class PackageSummary(packageName: String, usageCount: Int, versions: Map[String, Int])
+case class PackageSummary(packageName: String, usageCount: Int, versions: Map[String, List[String]])
 case class CompiledPackageList(items: List[PackageSummary])
 
 object CompiledPackageList:
@@ -18,15 +18,19 @@ object CompiledPackageList:
 
     def fromFiles(files: List[ComposerFile]): CompiledPackageList = {
         val compiledList = files
-            .flatMap(_.combinedList)
-            .groupBy(_.packageName)
-            .map((packageName, packageList) => {
-                val versionCounts = packageList
-                    .groupBy(_.packageVersion)
-                    .map((version, packages) => (version, packages.size))
+            .flatMap(file => file.combinedList.map(pkg => (file.name, pkg)))
+            .groupBy((_, pkg) => pkg.packageName)
+            .map { (packageName, packageList) =>
+                val versions = packageList
+                    .groupBy { (_, pkg) => pkg.packageVersion }
+                    .map { (version, packages) =>
+                        // Collect all filenames where this version appears
+                        (version, packages.map { case (fileName, _) => fileName })
+                    }
 
-                PackageSummary(packageName, packageList.size, versionCounts)
-            })
+                // Create the PackageSummary using the package name, the count of packages, and versions with filenames
+                PackageSummary(packageName, packageList.size, versions)
+            }
 
         CompiledPackageList(compiledList.toList)
     }
@@ -35,6 +39,18 @@ object CompiledPackageList:
         println(s"total number of files: $numberOfFiles")
         println(s"total number of packages (by name): $packageCount")
         println(s"total number of unique package versions: $versionCount")
+    }
+
+    def print(items: List[PackageSummary]): Unit = {
+        items.foreach((item: PackageSummary) => {
+            println(s"${item.packageName} - ${item.usageCount}")
+            item.versions.foreach((version, projectList) => {
+                println(s"  $version")
+                projectList.foreach((project) => {
+                    println(s"    $project")
+                })
+            })
+        })
     }
 
 case class ComposerFile(
--- src/main/worksheets/composer-worksheet.sc
+++ src/main/worksheets/composer-worksheet.sc
@@ -9,12 +9,7 @@ val repo = new ComposerRepository()
 val files = repo.all()
 
 val compiledList = CompiledPackageList.fromFiles(files)
-
 val sortedList = CompiledPackageList.sort(compiledList.items)
 
 CompiledPackageList.printInfo(files.size, compiledList.items.size, compiledList.items.flatMap(_.versions.keys).size)
-
-sortedList.foreach((item: PackageSummary) => {
-    println(s"${item.packageName} - ${item.usageCount}")
-    item.versions.foreach((version, count) => println(s"  $version - $count"))
-})
+CompiledPackageList.print(sortedList)
